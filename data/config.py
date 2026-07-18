ab21b2f538ed6ec83189f6fd6b48c1564a16f099
cotabnak-jpg <co.tabnak@gmail.com>
2026-07-18 08:40:21 +0330

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,13 @@
+import os
+
+# توکن ربات تلگرام
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# شناسه مدیران ربات
+# بعداً Chat ID افراد مجاز را اینجا قرار می‌دهیم
+ADMIN_IDS = [
+    123456789
+]
+
+# نام دیتابیس
+DATABASE_NAME = "data.db"
